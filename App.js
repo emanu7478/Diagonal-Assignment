d8c312317b99adfc15d9a7c345802e71becefb95
emanu7478 <emanu7478@gmail.com>
2022-01-22 12:23:35 +0530

    setup apllication boiler plate

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <View />
+    </View>
+  );
+}
+
+export default App;
---
+++ src/constants/colors.js
@@ -0,0 +1,4 @@
+export const PRIMARY = '#1e1f25';
+export const WHITE = '#FFFFFF';
+export const LIGHT_GREY = '#DADADA';
+export const SNOW_WHITE = '#F2F4F9';
