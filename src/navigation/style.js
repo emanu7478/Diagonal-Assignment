15d531c3909672a1ad8ba4e4d960b6041dd8c691
emanu7478 <emanu7478@gmail.com>
2022-01-23 23:30:32 +0530

    add keystore file and setup for production release for android

---
+++ src/navigation/style.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+import * as colors from '@constants/colors';
+
+export default StyleSheet.create({
+  loaderContainer: {
+    flex: 1,
+    backgroundColor: colors.PRIMARY,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
